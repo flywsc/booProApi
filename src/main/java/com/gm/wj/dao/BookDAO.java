d631103f82119b4f8cf00408600c2751beaa5987
wsc <https://github.com/flywsc/booProApi.git>
2020-09-22 11:51:05 +0800

    测试提交

--- src/main/java/com/gm/wj/dao/BookDAO.java
+++ src/main/java/com/gm/wj/dao/BookDAO.java
@@ -12,5 +12,6 @@ import java.util.List;
  */
 public interface BookDAO extends JpaRepository<Book,Integer> {
     List<Book> findAllByCategory(Category category);
+
     List<Book> findAllByTitleLikeOrAuthorLike(String keyword1, String keyword2);
 }
